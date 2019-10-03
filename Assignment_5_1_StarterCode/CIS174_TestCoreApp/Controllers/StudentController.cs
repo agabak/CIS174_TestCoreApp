6ccda9bfe1d09029f3cbea30e9586a42564638a2
Agaba Kabunga <agaba_k@hotmail.com>
2019-10-02 22:38:23 -0500

    Add Assigment drop down menu

---
+++ Assignment_5_1_StarterCode/CIS174_TestCoreApp/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CIS174_TestCoreApp.Models;
+
+namespace CIS174_TestCoreApp.Controllers
+{
+   
+    public class StudentController: Controller
+    {
+        public IActionResult Index([FromRoute]int id)
+        {
+            var studentsList = new List<StudentViewModel>
+            {
+                new StudentViewModel {FirstName ="John", LastName = "Doo", Grade = 54},
+                new StudentViewModel {FirstName ="Juma", LastName = "Ally", Grade = 67},
+                new StudentViewModel {FirstName ="Hammis", LastName = "Jullies", Grade = 70},
+                new StudentViewModel {FirstName ="Mike", LastName = "Hilty", Grade = 80}
+            };
+
+            var viewModel = new ViewModel
+                            {
+                                AccessLevel = id,
+                                Students = studentsList
+                            };
+            
+            return View(viewModel);
+        }
+    }
+}
---
+++ Assignment_5_1_StarterCode/CIS174_TestCoreApp/Models/ViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CIS174_TestCoreApp.Models
+{
+    public class ViewModel
+    {
+        public IEnumerable<StudentViewModel> Students { get; set; }
+        public int AccessLevel { get; set; }
+    }
+}
