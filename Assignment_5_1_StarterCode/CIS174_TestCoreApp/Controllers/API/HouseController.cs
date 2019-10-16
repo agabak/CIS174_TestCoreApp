8c31ad013cb34b04baff2a03102992d28f606b39
Agaba Kabunga <agaba_k@hotmail.com>
2019-10-15 21:44:17 -0500

    For this I would like to have them build a person type API with gets and posts. On the post I would like them to return the data that was posted to them in the response along with the status code so that I can see that they are accepting the posted data correctly.

---
+++ Assignment_5_1_StarterCode/CIS174_TestCoreApp/Controllers/API/HouseController.cs
@@ -0,0 +1,61 @@
+﻿using CIS174_TestCoreApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CIS174_TestCoreApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HouseController : Controller
+    {
+        public IEnumerable<HouseViewModel> Houses { get; } = new List<HouseViewModel>
+                                                {
+                                                    new HouseViewModel {
+                                                                         Id = 1,
+                                                                         Bedrooms = 4 ,
+                                                                         SquareFeet =  1854,
+                                                                         DateBuilt = new DateTime(1972, 5, 28),
+                                                                         Flooring = "Carpet"},
+                                                    new HouseViewModel {
+                                                                         Id = 2,
+                                                                         Bedrooms = 3,
+                                                                         SquareFeet =  1675,
+                                                                         DateBuilt = new DateTime(2015,10, 17),
+                                                                         Flooring = "Hardwood"}
+                                                };
+
+        public HouseController()
+        {
+                
+        }
+
+        [HttpGet]
+        public IEnumerable<HouseViewModel> Get()
+        {
+            return Houses;
+        }
+        
+        [HttpGet("{id}")]
+        public HouseViewModel Get(int id)
+        {
+            var house = Houses.FirstOrDefault(x => x.Id == id);
+            if(house != null) return house;
+
+            return null;
+        }
+
+        [HttpPost("{create}")]
+        public IActionResult Post(HouseViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                return StatusCode(201);
+            }
+
+            return BadRequest();
+        }
+    }
+}
---
+++ Assignment_5_1_StarterCode/CIS174_TestCoreApp/Models/HouseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CIS174_TestCoreApp.Models
+{
+    public class HouseViewModel
+    {
+        public int Id { get; set; }
+        public int Bedrooms { get; set; }
+        public int SquareFeet { get; set; }
+        public DateTime DateBuilt { get; set; }
+        public string Flooring { get; set; }
+    }
+}
